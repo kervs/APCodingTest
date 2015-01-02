9cefa4d89b5df0179bb510c1b4b783abc2f74f71
kervs <kervins.valcourt@gmail.com>
2015-01-02 01:59:52 -0500

    spin action

--- IOSProgrammerTest/Animation Section/AnimationSectionViewController.m
+++ IOSProgrammerTest/Animation Section/AnimationSectionViewController.m
@@ -8,8 +8,11 @@
 
 #import "AnimationSectionViewController.h"
 #import "MainMenuViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface AnimationSectionViewController ()
+@property (strong, nonatomic) IBOutlet UITextView *txtFieldView;
+@property (strong, nonatomic) IBOutlet UIImageView *logo;
 
 @end
 
@@ -19,6 +22,32 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+}
+- (IBAction)spinBtnDidPressed:(id)sender {
+    NSArray *colorArray = @[[UIColor redColor],[UIColor blueColor],[UIColor purpleColor],[UIColor cyanColor],[UIColor orangeColor],[UIColor magentaColor]];
+    static int colorIndex = 0;
+    NSInteger numberOfSpins = 2;
+    
+    while (numberOfSpins != 0) {
+        [UIView animateWithDuration:0.6
+                              delay:0
+                            options:UIViewAnimationOptionCurveLinear
+                         animations:^{
+                             [self.logo setTransform:CGAffineTransformRotate(self.logo.transform, M_PI)];
+                             
+                             // Change the background color
+                             self.logo.backgroundColor = [colorArray objectAtIndex:colorIndex];
+                             colorIndex = (colorIndex + 1) % colorArray.count;
+                         }
+                         completion:nil];
+        numberOfSpins--;
+    }
+    self.logo.backgroundColor = [UIColor clearColor];
+    
+    
+
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
